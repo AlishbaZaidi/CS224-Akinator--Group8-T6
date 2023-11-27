48c3c865217104eafabbb871884129d028c5dfef
Huzaifa Ahmed Khan <122218580+hak-huzaifa@users.noreply.github.com>
2023-11-27 20:41:40 +0500

    Add files via upload

---
+++ Extras.cpp
@@ -0,0 +1,20 @@
+#include"Extras.hpp"
+
+
+int NewRandomNumGenerator (int range)
+{
+    int num = (std::rand() % range)+1;
+    return num;
+}
+
+bool newisInArray(int value, const int* arr, int size)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            if (arr[i] == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
