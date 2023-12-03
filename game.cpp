05914963a2f1d3567fdae1a89de2cf26b76b7ba6
Huzaifa Ahmed Khan <122218580+hak-huzaifa@users.noreply.github.com>
2023-12-03 22:38:23 +0500

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,199 +1,225 @@
-#include "game.hpp"
+#include "game.hpp"  // Include the header file for the Game class
 
+// Constructor definition for the Game class
 Game::Game() : window(nullptr), renderer(nullptr), currentTexture(nullptr), running(true), userWantsMusic(false) {
-    initSDL();
+    initSDL();  // Initialize SDL upon creating a Game instance
 }
 
+// Destructor definition for the Game class
 Game::~Game() 
 {
-    // if(currentTexture)
-    // {
-    //     SDL_DestroyTexture(currentTexture);
-    //     currentTexture = nullptr;
-    // }
-    
-    // if (renderer)
-    // {
-    //     SDL_DestroyRenderer(renderer);
-    // }
-    // if (window)
-    // {
-    //     SDL_DestroyWindow(window);
-    //     window = nullptr;
-    // }
-    // IMG_Quit();
-    // SDL_Quit();
-    closeSDL();
+    closeSDL(); // Clean up SDL using the closeSDL function
 }
 
+// Function to initialize SDL and related libraries
 void Game::initSDL() {
-    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        // Handle SDL initialization error
+    // Initialize SDL_mixer for audio
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
     }
 
+    // Initialize SDL for video
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        return; // Early return in case of initialization failure
+    }
+
+    // Create an SDL window
     window = SDL_CreateWindow("HU-Quest", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
     if (!window) {
-        // Handle window creation error
+        std::cerr << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        SDL_Quit(); // Clean up SDL in case of window creation failure
+        return; // Early return in case of failure
     }
 
+    // Create an SDL renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        // Handle renderer creation error
+        std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window); // Clean up the window
+        SDL_Quit(); // Clean up SDL
+        return; // Early return in case of failure
     }
 
-    IMG_Init(IMG_INIT_PNG);
+    // Initialize SDL_image for handling PNG images
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer); // Clean up the renderer
+        SDL_DestroyWindow(window); // Clean up the window
+        SDL_Quit(); // Clean up SDL
+        return; // Early return in case of failure
+    }
 }
 
+// Function to close and clean up SDL
 void Game::closeSDL() {
     if (currentTexture) {
-        SDL_DestroyTexture(currentTexture);
+        SDL_DestroyTexture(currentTexture); // Destroy the current texture if it exists
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
+    SDL_DestroyRenderer(renderer); // Destroy the renderer
+    SDL_DestroyWindow(window); // Destroy the window
+    IMG_Quit(); // Quit SDL_image
+    SDL_Quit(); // Quit SDL
 }
 
+// Function to load an image as a texture
 void Game::loadImage(const std::string& imagePath) {
-    SDL_Surface* loadedSurface = IMG_Load(imagePath.c_str());
+    SDL_Surface* loadedSurface = IMG_Load(imagePath.c_str()); // Load the image as a surface
     if (!loadedSurface) {
-        // Handle image loading error
+        std::cerr << "Unable to load image " << imagePath << "! SDL_image Error: " << IMG_GetError() << std::endl;
+        return; // Early return in case of loading failure
     } else {
-        SDL_Texture* newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        SDL_Texture* newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface); // Create a texture from the surface
         if (!newTexture) {
-            // Handle texture creation error
+            std::cerr << "Unable to create texture from " << imagePath << "! SDL Error: " << SDL_GetError() << std::endl;
+            SDL_FreeSurface(loadedSurface); // Free the loaded surface
+            return; // Early return in case of texture creation failure
         }
 
+        // Replace the current texture with the new texture
         if (currentTexture) {
-            SDL_DestroyTexture(currentTexture);
+            SDL_DestroyTexture(currentTexture); // Destroy the old texture if it exists
         }
-        currentTexture = newTexture;
-        SDL_FreeSurface(loadedSurface);
+        currentTexture = newTexture; // Set the new texture as the current texture
+
+        SDL_FreeSurface(loadedSurface); // Free the loaded surface
     }
 }
 
+// Function to render the current texture
 void Game::render() {
-    SDL_RenderClear(renderer);
+    SDL_RenderClear(renderer); // Clear the current rendering target
     if (currentTexture) {
-        SDL_RenderCopy(renderer, currentTexture, NULL, NULL);
+        SDL_RenderCopy(renderer, currentTexture, NULL, NULL); // Copy the texture to the rendering target
     }
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer); // Update the screen with rendering performed
 }
 
+// Function to render the initial screen of the game
 void Game::renderInitialScreen()
 {
-    loadImage("Start.png");
-    render();
+    loadImage("Start.png"); // Load the start screen image
+    render(); // Render the start screen
 }
 
+// Function to handle SDL events
 void Game::handleEvents() {
-    SDL_Event e;
-    while (SDL_PollEvent(&e) != 0) {
-        if (e.type == SDL_QUIT) {
-            running = false;
+    SDL_Event e; // Event variable
+    while (SDL_PollEvent(&e) != 0) { // Poll for pending events
+        if (e.type == SDL_QUIT) { // Check for quit event
+            running = false; // Set the running flag to false
         }
     }
 }
 
+// Function to check if the game is still running
 bool Game::isRunning() const {
-    return running;
+    return running; // Return the running state
 }
 
-
+// Function to run the game loop
 void Game::run()
 {
-    renderInitialScreen();
+    renderInitialScreen(); // Render the initial screen
 
-    bool king = promptMusicChoice();
+    bool king = promptMusicChoice(); // Prompt the user for music choice
     
-    if (king) 
-    {
-        startMusic();
-        // MG.playMusic("Music.mp3");
+    if (king) {
+        startMusic(); // Start music if the user chooses
     }
 
-    bool clicked = false;
-    SDL_Event e;
+    bool clicked = false; // Flag to check if the user has clicked
+    SDL_Event e; // Event variable
     while (!clicked)
     {
         while(SDL_PollEvent(&e) != 0)
         {
             if(e.type == SDL_QUIT)
             {
-                running = false;
-                return;
+                running = false; // Set the running flag to false
+                return; // Exit the function
             }else if(e.type == SDL_MOUSEBUTTONDOWN)
             {
-                clicked = true;
+                clicked = true; // Set the clicked flag to true
             }
         }
     }
 }
 
-
-
+// Function to display the outcome of the game
 void Game::displayOutcome(const std::string& outcomeImage) {
-    loadImage(outcomeImage); 
-    render();
+    loadImage(outcomeImage); // Load the outcome image
+    render(); // Render the outcome image
     if (userWantsMusic) {
-        // MG.playMusic("Outro.mp3");
         MusicManager& MG = MusicManager::getInstance();
-        MG.playMusic("Outro.mp3"); 
+        MG.playMusic("Outro.mp3"); // Play outro music if user wants music
     }
-    // Optionally wait for a user input or a set amount of time before proceeding
-    SDL_Delay(5000); // For example, wait for 3 seconds
+    SDL_Delay(5000); // Delay for 5 seconds to display the outcome
 }
 
-
-
+// Function to prompt the user for a music choice
 bool Game::promptMusicChoice() {
+    // Create a new window for the music choice prompt
     SDL_Window* choiceWindow = SDL_CreateWindow("Music Choice", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, SDL_WINDOW_SHOWN);
+    // Create a renderer for the choice window
     SDL_Renderer* choiceRenderer = SDL_CreateRenderer(choiceWindow, -1, SDL_RENDERER_ACCELERATED);
+    // Load the popup image
     SDL_Surface* loadedSurface = IMG_Load("Popup.png");
+    // Create a texture from the loaded surface
     SDL_Texture* popupTexture = SDL_CreateTextureFromSurface(choiceRenderer, loadedSurface);
-    SDL_FreeSurface(loadedSurface); // Free the surface after creating the texture
+    // Free the loaded surface
+    SDL_FreeSurface(loadedSurface);
 
-    bool choiceMade = false;
-    SDL_Event e;
+    bool choiceMade = false; // Flag to indicate if a choice has been made
+    SDL_Event e; // Event variable
 
     while (!choiceMade) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
-                exit(0);
+                exit(0); // Exit the program if quit event is detected
             } else if (e.type == SDL_MOUSEBUTTONDOWN) {
-                int x, y;
-                SDL_GetMouseState(&x, &y);
-                if (x >= 107 && x <= 167 && y >= 68 && y <= 131) { // YES coordinates
-                    userWantsMusic = true;
-                    // MG.playMusic("Music.mp3"); // Replace with correct path
-                    choiceMade = true;
-                } else if (x >= 30 && x <= 90 && y >= 68 && y <= 131) { // NO coordinates
-                    userWantsMusic = false;
+                int x, y; // Variables to store mouse coordinates
+                SDL_GetMouseState(&x, &y); // Get the current mouse state
+                // Check if the YES button coordinates are clicked
+                if (x >= 107 && x <= 167 && y >= 68 && y <= 131) {
+                    userWantsMusic = true; // Set the music preference to true
+                    choiceMade = true; // Set the choice made flag to true
+                } else if (x >= 30 && x <= 90 && y >= 68 && y <= 131) { // Check if the NO button coordinates are clicked
+                    userWantsMusic = false; // Set the music preference to false
                     stopMusic(); // Stop music if playing
-                    choiceMade = true;
+                    choiceMade = true; // Set the choice made flag to true
                 }
             }
         }
-        SDL_RenderClear(choiceRenderer);
-        SDL_RenderCopy(choiceRenderer, popupTexture, NULL, NULL);
-        SDL_RenderPresent(choiceRenderer);
+        SDL_RenderClear(choiceRenderer); // Clear the renderer
+        SDL_RenderCopy(choiceRenderer, popupTexture, NULL, NULL); // Copy the texture to the renderer
+        SDL_RenderPresent(choiceRenderer); // Present the renderer
     }
 
+    // Clean up the texture, renderer, and window
     SDL_DestroyTexture(popupTexture);
     SDL_DestroyRenderer(choiceRenderer);
     SDL_DestroyWindow(choiceWindow);
 
-    return userWantsMusic;
+    return userWantsMusic; // Return the user's music preference
 }
 
+// Function to start playing music
 void Game::startMusic() {
     if (userWantsMusic) {
-        MusicManager::getInstance().playMusic("Music.mp3");  // Add correct path
+        MusicManager::getInstance().playMusic("Music.mp3");  // playing music
+    }
+}
+
+// Overloaded function
+void Game::startMusic(std::string filename) {
+    if (userWantsMusic) {
+        const char* filenameCStr = filename.c_str();
+        MusicManager::getInstance().playMusic(filenameCStr);  // playing music specified by filename
     }
 }
 
 void Game::stopMusic() {
-    MusicManager::getInstance().stopMusic();
+    MusicManager::getInstance().stopMusic(); // stopping music
 }
--- game.hpp
+++ game.hpp
@@ -1,79 +1,57 @@
-// Include necessary SDL and C++ headers
+// This line is a header guard to prevent multiple inclusions of this header file.
 #pragma once
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include "MusicManager.hpp"
 
-// Define the Game class
+// Including necessary SDL libraries and standard I/O libraries.
+#include <SDL.h>             // Main SDL header
+#include <SDL_image.h>       // SDL extension for handling image files
+#include <stdio.h>           // Standard input/output library
+#include <iostream>          // Library for console input and output
+#include <string>            // String type header
+#include "MusicManager.hpp"  // Including the MusicManager class header
+
+// Declaration of the Game class
 class Game {
 public:
-    // Singleton pattern: Get the instance of the Game class
+    // Singleton pattern implementation for the Game class.
     static Game& getInstance() 
     {
-        static Game instance;
-        return instance;
+        static Game instance; // Create a static instance of Game
+        return instance;      // Return the instance
     }
 
-    // Delete copy constructor and copy assignment operator to enforce singleton pattern
+    // Delete the copy constructor to prevent copying of the singleton instance
     Game(Game const&) = delete;
+    // Delete the assignment operator to prevent assignment of the singleton instance
     void operator=(Game const&) = delete;
 
-    // Destructor for the Game class
+    // Destructor of the Game class
     ~Game();
-    
-    // Flag indicating whether the user wants music
-    bool userWantsMusic;
 
-    // Function to load an image as a texture
-    void loadImage(const std::string& imagePath);
-
-    // Function to render the current texture to the screen
-    void render();
-
-    // Function to handle SDL events
-    void handleEvents();
-
-    // Function to check if the game is running
-    bool isRunning() const;
-
-    // Function to render the initial screen
-    void renderInitialScreen();
-
-    // Function to run the main game loop
-    void run();
-
-    // Function to display the outcome image and play outro music
-    void displayOutcome(const std::string& outcomeImage);
-
-    // Function to prompt the user for their music choice
-    bool promptMusicChoice();
-
-    // Function to start playing music
-    void startMusic();
-
-    // Function to stop playing music
-    void stopMusic();
-
-    // Function to release SDL resources
-    void closeSDL();
+    // Member variables and functions
+    // MusicManager MG;               // Uncomment to use MusicManager
+    bool userWantsMusic;            // Flag to store if the user wants music
+    void loadImage(const std::string& imagePath); // Function to load an image from a path
+    void render();                                // Function to render the current image
+    void handleEvents();                          // Function to handle SDL events
+    bool isRunning() const;                       // Function to check if the game is running
+    void renderInitialScreen();                   // Function to render the initial screen
+    void run();                                   // Function to run the game
+    void displayOutcome(const std::string& outcomeImage); // Function to display the outcome image
+    bool promptMusicChoice();                     // Function to prompt the user for music choice
+    void startMusic();                            // Function to start playing music
+    void startMusic(std::string filenaem);         // Overload Function to start playing music
+    void stopMusic();                             // Function to stop playing music
+    void closeSDL();                              // Function to close SDL and clean up
 
 private:
-    // Private constructor for the singleton pattern
+    // Constructor is private to enforce singleton pattern
     Game();
+    SDL_Window* window;            // Pointer to the SDL_Window
+    SDL_Renderer* renderer;        // Pointer to the SDL_Renderer
+    SDL_Texture* currentTexture;   // Pointer to the current SDL_Texture
+    bool running;                  // Flag to store the running state of the game
 
-    // SDL window and renderer
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-
-    // Current texture to be rendered
-    SDL_Texture* currentTexture;
-
-    // Flag indicating whether the game is running
-    bool running;
-
-    // Function to initialize SDL and related libraries
-    void initSDL();
+    // Private member functions
+    void initSDL();                // Function to initialize SDL
+    // void destroyTextures();      // Uncomment to use a function to destroy textures
 };
