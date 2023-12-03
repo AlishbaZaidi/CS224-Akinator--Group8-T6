e151b536e17c01d7b3b95eaf93b9f122b9b950d1
Huzaifa Ahmed Khan <122218580+hak-huzaifa@users.noreply.github.com>
2023-12-03 20:12:27 +0500

    Add files via upload

--- Extras.cpp
+++ Extras.cpp
@@ -1,13 +1,18 @@
 #include"Extras.hpp"
 
-
+// Function to generate a new random number within a given range
 int NewRandomNumGenerator (int range)
 {
+    // Generate a random number modulo the range and add 1 for [1, range]
     int num = (std::rand() % range)+1;
+    // Return the generated number
     return num;
 }
 
+// Function to check if a value is present in an array
 bool newisInArray(int value, const int* arr, int size)
+
+    // Iterate over the array to find the value
     {
         for (int i = 0; i < size; ++i)
         {
--- Extras.hpp
+++ Extras.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include<iostream>
 
-
-
+// Declaration of the newisInArray function
 bool newisInArray(int value, const int* arr, int size);
 
+// Declaration of the NewRandomNumGenerator function
 int NewRandomNumGenerator (int range);
