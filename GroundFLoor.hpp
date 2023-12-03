17d1e16cb2e107e5937f1df83040ed01784c0f6c
Huzaifa Ahmed Khan <122218580+hak-huzaifa@users.noreply.github.com>
2023-12-03 20:49:42 +0500

    Add files via upload

--- GroundFLoor.hpp
+++ GroundFLoor.hpp
@@ -1,34 +1,76 @@
+// This line is a header guard to prevent multiple inclusions of this header file.
 #pragma once
-#include<string>
-#include<iostream>
-#include"Locations.hpp"
-#include"Extras.hpp"
 
+// Including necessary header files.
+#include<string>  // For using the std::string type
+#include<iostream> // For input and output operations
+#include"Locations.hpp" // Including the Locations base class header
+#include"Extras.hpp"    // Including additional necessary declarations
+
+// Declaration of the GroundFloor class which inherits publicly from the Locations class
 class GroundFloor : public Locations
 {
 public:
+    // Member variable 'option' of type std::string to store the player's choice
     std::string option;
     
-    // GroundFloor() = default;
+    // The following lines declare member functions of the GroundFloor class.
+    // Each function takes a reference to a Game object and returns a boolean value.
 
+    // Function to determine if the player's location is in the west direction
     bool westDirection(Game& game);
-    bool northDirection(Game& game);
-    bool centralDirection(Game& game);
-    bool courtyard(Game& game);
-    bool classroom(Game& game);
-    bool multiStory(Game& game);
-    bool famousPerson(Game& game);
-    bool airCourt(Game& game);
-    bool waterCourt(Game& game);
-    bool fireCourt(Game& game);
-    bool earthCourt(Game& game);
-    bool comfortZone(Game& game);
-    bool hygenicFood(Game& game);
-    bool lectureHall(Game& game);
-    bool multipleOffices(Game& game);
-    bool eventsKarao(Game& game);
-    bool wellness(Game& game);
-    bool careerOffice(Game& game);
-    void questionOrder();
 
+    // Function to determine if the player's location is in the north direction
+    bool northDirection(Game& game);
+
+    // Function to determine if the player's location is central
+    bool centralDirection(Game& game);
+
+    // Function to check if the player is thinking of a courtyard
+    bool courtyard(Game& game);
+
+    // Function to check if the player is thinking of a classroom
+    bool classroom(Game& game);
+
+    // Function to determine if the player's location is a multi-story building
+    bool multiStory(Game& game);
+
+    // Function to check if the location is named after a famous person
+    bool famousPerson(Game& game);
+
+    // Function to determine if the player is thinking of the Air Court
+    bool airCourt(Game& game);
+
+    // Function to determine if the player is thinking of the Water Court
+    bool waterCourt(Game& game);
+
+    // Function to determine if the player is thinking of the Fire Court
+    bool fireCourt(Game& game);
+
+    // Function to determine if the player is thinking of the Earth Court
+    bool earthCourt(Game& game);
+
+    // Function to check if the player is thinking of a comfort zone (like a lounge)
+    bool comfortZone(Game& game);
+
+    // Function to check if the location is known for hygienic food
+    bool hygenicFood(Game& game);
+
+    // Function to determine if the player is thinking of a lecture hall
+    bool lectureHall(Game& game);
+
+    // Function to check if the location contains multiple offices
+    bool multipleOffices(Game& game);
+
+    // Function to determine if the location is used for event discussions
+    bool eventsKarao(Game& game);
+
+    // Function to check if the location is dedicated to wellness
+    bool wellness(Game& game);
+
+    // Function to determine if the location is a career office
+    bool careerOffice(Game& game);
+
+    // Function to determine the order of questions asked to the player
+    void questionOrder();
 };
--- GroundFloor.cpp
+++ GroundFloor.cpp
@@ -2,39 +2,55 @@
 #include <iostream>
 #include "GroundFloor.hpp"
 
-
-
+// Define a member function 'westDirection' for the class 'GroundFloor'
 bool GroundFloor::westDirection(Game& game)
 {
-    //std::cout << "Is the location you are thinking in West?" << "\n";
-    //std::cin >> option;
+    // Load an image with the filename 'QWest.png' using the 'game' object
     game.loadImage("QWest.png");
-        game.render();
+    // Render the loaded image onto the screen
+    game.render();
 
-        SDL_Event e;
-        while (true) 
+    // Declare an SDL_Event variable to handle events
+    SDL_Event e;
+    // Start an infinite loop to continuously check for events
+    while (true) 
+    {
+        // Check if there are any pending events
+        if (SDL_PollEvent(&e) != 0) 
         {
-            if (SDL_PollEvent(&e) != 0) 
+            // Check if the event is of type SDL_QUIT (e.g., closing the window)
+            if (e.type == SDL_QUIT) 
             {
-                if (e.type == SDL_QUIT) 
+                // Exit the program
+                exit(0);
+            } 
+            // Check if the event is a mouse button down event
+            else if (e.type == SDL_MOUSEBUTTONDOWN) 
+            {
+                // Declare variables to store the x and y coordinates of the mouse click
+                int x, y;
+                // Get the current mouse state (position)
+                SDL_GetMouseState(&x, &y);
+                // Check if the mouse click is within the coordinates for the 'YES' option
+                if (x >= 135 && x <= 323 && y >= 470 && y <= 529) 
                 {
-                    exit(0);
-                } else if (e.type == SDL_MOUSEBUTTONDOWN) 
+                    // Set the option to 'yes'
+                    option = "yes";
+                    // Break out of the loop as the event is handled
+                    break;
+                } 
+                // Check if the mouse click is within the coordinates for the 'NO' option
+                else if (x >= 457 && x <= 646 && y >= 470 && y <= 529) 
                 {
-                    int x, y;
-                    SDL_GetMouseState(&x, &y);
-                    if (x >= 135 && x <= 323 && y >= 470 && y <= 529) 
-                    { // YES coordinates
-                        option = "yes";
-                        break;
-                    } else if (x >= 457 && x <= 646 && y >= 470 && y <= 529) 
-                    { // NO coordinates
-                        option = "no";
-                        break;
-                    }
+                    // Set the option to 'no'
+                    option = "no";
+                    // Break out of the loop as the event is handled
+                    break;
                 }
             }
         }
+    }
+    // Return true if the option is 'yes', else return false
     if (option == "yes")
     {
         return true;
@@ -44,6 +60,7 @@ bool GroundFloor::westDirection(Game& game)
         return false;
     }        
 };
+
 
 bool GroundFloor::northDirection(Game& game){
     //std::cout << "Is the location you are thinking in North?" << "\n";
@@ -206,8 +223,7 @@ bool GroundFloor::classroom(Game& game){
 };
 
 bool GroundFloor::multiStory(Game& game){
-    //std::cout << "Is your location found on multiple floors?" << "\n";
-    //std::cin >> option;
+    
     game.loadImage("Qmultiple.png");
     game.render();
 
@@ -724,99 +740,129 @@ bool GroundFloor::careerOffice(Game& game){
     }
 }
 
-
+// This function determines the player's location based on their responses to a series of questions.
 void GroundFloor::questionOrder()
 {
+    // Retrieve a reference to the Game singleton instance.
     Game& game = Game::getInstance();
+
+    // Determine if the player's thought direction is west.
     bool west = westDirection(game);
     if(west){
+        // Generate a random number (0 or 1) for decision branching.
         int num1 = NewRandomNumGenerator(2);
         if(num1){
+            // Check if the player is thinking of a courtyard.
             bool court = courtyard(game);
             if(court){
+                // Check if the player is thinking of the Air Courtyard.
                 bool air = airCourt(game);
                 if(air){
+                    // Display the outcome for the Air Courtyard.
                     game.displayOutcome("Air.png");
-                    std::cout<<"You're thinking of Air Courtyard";
+                    std::cout << "You're thinking of Air Courtyard";
                 }
                 else{
+                    // Check if the player is thinking of the Earth Courtyard.
                     bool earth = earthCourt(game);
                     if(earth){
+                        // Display the outcome for the Earth Courtyard.
                         game.displayOutcome("Earth.png");
-                        std::cout<<"You're thinking of Earth Courtyard";
+                        std::cout << "You're thinking of Earth Courtyard";
                     }
                     else{
+                        // Generate another random number for further decision branching.
                         int num2 = NewRandomNumGenerator(2);
                         if(num2){
+                            // Check if the player is thinking of the Water Courtyard.
                             bool water = waterCourt(game);
                             if(water){
+                                // Display the outcome for the Water Courtyard.
                                 game.displayOutcome("Water.png");
-                                std::cout<<"You're thinking of Water Courtyard";
+                                std::cout << "You're thinking of Water Courtyard";
                             }
                             else{
+                                // If not Water, then it must be Fire Courtyard by elimination.
                                 game.displayOutcome("Fire.png");
-                                std::cout<<"You're thinking of Fire Courtyard";
+                                std::cout << "You're thinking of Fire Courtyard";
                             }
                         }
                         else{
+                            // Check if the player is thinking of the Fire Courtyard.
                             bool fire = fireCourt(game);
                             if(fire){
+                                // Display the outcome for the Fire Courtyard.
                                 game.displayOutcome("Fire.png");
-                                std::cout<<"You're thinking of Fire Courtyard";
+                                std::cout << "You're thinking of Fire Courtyard";
                             }
                             else{
+                                // If not Fire, then it must be Water Courtyard by elimination.
                                 game.displayOutcome("Water.png");
-                                std::cout<<"You're thinking of Water Courtyard";
+                                std::cout << "You're thinking of Water Courtyard";
                             }
                         }
                     }
                 }
             }
             else{
+                // If not a courtyard, then it must be the Wellness Center by elimination.
                 game.displayOutcome("Wellness.png");
-                std::cout<<"You're thinking of Wellness Center";
+                std::cout << "You're thinking of Wellness Center";
             }
         }
         else{
+            // Check if the player is thinking of the Wellness Center.
             bool well = wellness(game);
             if(well){
+                // Display the outcome for the Wellness Center.
                 game.displayOutcome("Wellness.png");
-                std::cout<<"You're thinking of Wellness Center";
+                std::cout << "You're thinking of Wellness Center";
             }
             else{
+                // Check if the player is thinking of the Fire Courtyard.
                 bool fire = fireCourt(game);
                 if(fire){
+                    // Display the outcome for the Fire Courtyard.
                     game.displayOutcome("Fire.png");
-                    std::cout<<"You're thinking of Fire Courtyard";
+                    std::cout << "You're thinking of Fire Courtyard";
                 }
                 else{
+                    // Check if the player is thinking of the Water Courtyard.
                     bool water = waterCourt(game);
                     if(water){
-                        game.displayOutcome("Fire.png");
-                        std::cout<<"You're thinking of Water Courtyard";
+                        // If Water, display the outcome for the Water Courtyard.
+                        game.displayOutcome("Fire.png");  // Note: Should this be "Water.png"?
+                        std::cout << "You're thinking of Water Courtyard";
                     }
                     else{
+                        // Generate another random number for further decision branching.
                         int num3 = NewRandomNumGenerator(2);
                         if(num3){
+                            // Check if the player is thinking of the Air Courtyard.
                             bool air = airCourt(game);
                             if(air){
+                                // Display the outcome for the Air Courtyard.
                                 game.displayOutcome("Air.png");
-                                std::cout<<"You're thinking of Air Courtyard";
+                                std::cout << "You're thinking of Air Courtyard";
                             }
                             else{
+                                // If not Air, then it must be Earth Courtyard by elimination.
                                 game.displayOutcome("Earth.png");
-                                std::cout<<"You're thinking of Earth Courtyard";
+                                std::cout << "You're thinking of Earth Courtyard";
                             }
                         }
                         else{
+                            // Check if the player is thinking of the Earth Courtyard.
                             bool earth = earthCourt(game);
                             if(earth){
+                                // Display the outcome for the Earth Courtyard.
                                 game.displayOutcome("Earth.png");
-                                std::cout<<"You're thinking of Earth Courtyard";
+                                std::cout << "You're thinking of Earth Courtyard";
                             }
                             else{
+                                // If not Earth, then it must be Air Courtyard by elimination.
                                 game.displayOutcome("Air.png");
-                                std::cout<<"You're thinking of Air Courtyard";
+                                std::cout << "You're thinking of Air Courtyard";
                             }
                         }
                     }
@@ -825,103 +871,131 @@ void GroundFloor::questionOrder()
         }
     }
     else{
+        // Determine if the player's thought direction is north.
         bool north = northDirection(game);
         if(north){
+            // Generate a random number for decision branching.
             int num3 = NewRandomNumGenerator(2);
             if(num3){
+                // Check if the player is thinking of the Office of Career and Services.
                 bool career = careerOffice(game);
                 if(career){
+                    // Display the outcome for the Office of Career and Services.
                     game.displayOutcome("OCS.png");
-                    std::cout<<"You're thinking of Office of Career and Services";
+                    std::cout << "You're thinking of Office of Career and Services";
                 }
                 else{
+                    // If not the career office, then it must be the Student Center by elimination.
                     game.displayOutcome("StudentCentre.png");
-                    std::cout<<"You're thinking of Student Center";
+                    std::cout << "You're thinking of Student Center";
                 }
             }
             else{
+                // Check if the player is thinking of the Student Center.
                 bool studentcen = multipleOffices(game);
                 if(studentcen){
+                    // Display the outcome for the Student Center.
                     game.displayOutcome("StudentCentre.png");
-                    std::cout<<"You're thinking of Student Center";
+                    std::cout << "You're thinking of Student Center";
                 }
                 else{
+                    // If not the Student Center, then it must be the Office of Career and Services by elimination.
                     game.displayOutcome("OCS.png");
-                    std::cout<<"You're thinking of Office of Career and Services";
+                    std::cout << "You're thinking of Office of Career and Services";
                 }
             }
         }
         else{
+            // Determine if the player's thought direction is central.
             bool central = centralDirection(game);
             if(central){
+                // Check if the player is thinking of a classroom.
                 bool classRoom = classroom(game);
                 if(classRoom){
+                    // Check if the player is thinking of a lecture hall.
                     bool hall = lectureHall(game);
                     if(hall){
+                        // Display the outcome for Soorty Hall.
                         game.displayOutcome("Soorty.png");
-                        std::cout<<"It's Soorty Hall";
+                        std::cout << "It's Soorty Hall";
                     }
                     else{
+                        // Check if the location is named after a famous person.
                         bool perosnality = famousPerson(game);
                         if(perosnality){
+                            // Display the outcome for Arif Habib Classroom.
                             game.displayOutcome("ArifHabib.png");
-                            std::cout<<"You're thinking of Arif Habib Classroom";
+                            std::cout << "You're thinking of Arif Habib Classroom";
                         }
                         else{
+                            // If not named after a famous person, then it must be the Multipurpose Room by elimination.
                             game.displayOutcome("MPR.png");
-                            std::cout<<"The location you're thinking is Multipurpose Room";
+                            std::cout << "The location you're thinking is Multipurpose Room";
                         }
                     }
-
                 }
                 else{
+                    // Generate a random number for further decision branching.
                     int num4 = NewRandomNumGenerator(2);
                     if(num4){
+                        // Check if the player is thinking of the Student Lounge.
                         bool lounge = comfortZone(game);
                         if(lounge){
+                            // Display the outcome for the Student Lounge.
                             game.displayOutcome("Studentlounge.png");
-                            std::cout<<"You're thinking of Student Lounge";
+                            std::cout << "You're thinking of Student Lounge";
                         }
                         else{
+                            // If not the lounge, then it must be Student Life by elimination.
                             game.displayOutcome("SL.png");
-                            std::cout<<"You're thinking of Student Life";
+                            std::cout << "You're thinking of Student Life";
                         }
                     }
                     else{
+                        // Check if the player is thinking of Student Life.
                         bool life = eventsKarao(game);
                         if(life){
+                            // Display the outcome for Student Life.
                             game.displayOutcome("SL.png");
-                            std::cout<<"You're thinking of Student Life";
+                            std::cout << "You're thinking of Student Life";
                         }
                         else{
+                            // If not Student Life, then it must be the Student Lounge by elimination.
                             game.displayOutcome("Studentlounge.png");
-                            std::cout<<"You're thinking of Student Lounge";
+                            std::cout << "You're thinking of Student Lounge";
                         }
                     }
                 }
             }
             else{
+                // Generate a random number for the final set of decision branching.
                 int num5 = NewRandomNumGenerator(2);
                 if(num5){
+                    // Check if the player is thinking of a multi-story building.
                     bool multi = multiStory(game);
                     if(multi){
+                        // Display the outcome for the Library.
                         game.displayOutcome("Lib.png");
-                        std::cout<<"You're thinking of Library";
+                        std::cout << "You're thinking of Library";
                     }
                     else{
+                        // If not multi-story, then it must be Cafe2Go by elimination.
                         game.displayOutcome("Cafe2go.png");
-                        std::cout<<"You're thinking of Cafe2Go";
+                        std::cout << "You're thinking of Cafe2Go";
                     }
                 }
                 else{
+                    // Check if the player is thinking of a location with hygienic food (Cafe2Go).
                     bool cafe = hygenicFood(game);
                     if(cafe){
+                        // Display the outcome for Cafe2Go.
                         game.displayOutcome("Cafe2go.png");
-                        std::cout<<"You're thinking of Cafe2Go";
+                        std::cout << "You're thinking of Cafe2Go";
                     }
                     else{
+                        // If not Cafe2Go, then it must be the Library by elimination.
                         game.displayOutcome("Lib.png");
-                        std::cout<<"You're thinking of Library";
+                        std::cout << "You're thinking of Library";
                     }
                 }
             }
